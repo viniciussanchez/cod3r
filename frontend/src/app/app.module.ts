c1b9510800138ebd97fb5c90d63854cb687910de
viniciussanchez <viniciuss.sanchez@gmail.com>
2020-07-03 08:09:34 -0300

    Product update and delete

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -31,6 +31,8 @@ import { ProductRead2Component } from './components/product/product-read2/produc
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
 
@@ -46,7 +48,9 @@ registerLocaleData(localePt);
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -22,7 +22,7 @@ export class ProductCreateComponent implements OnInit {
 
   createProduct(): void {
     this.productService.create(this.product).subscribe(() => {
-      this.productService.showMessage('Registro salvo com sucesso!');
+      this.productService.showMessage('Produto cadastrado com sucesso');
       this.router.navigate(['/products']);
     });
   }
