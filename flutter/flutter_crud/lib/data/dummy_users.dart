7719dedc485c7d01b9b9255fa5d11ad1bab78763
viniciussanchez <viniciuss.sanchez@gmail.com>
2020-07-21 17:53:18 -0300

    User data

---
+++ flutter/flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: 1,
+      name: "Vinicius",
+      email: "viniciuss.sanchez@gmail.com",
+      avatarUrl:
+          "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png"),
+  '2': const User(
+      id: 1,
+      name: "Ingrid",
+      email: "ingrid.morais@gmail.com",
+      avatarUrl:
+          "https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png"),
+  '3': const User(
+      id: 1,
+      name: "Sandra",
+      email: "sandra.cabrera@gmail.com",
+      avatarUrl:
+          "https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_960_720.png")
+};
---
+++ flutter/flutter_crud/lib/models/user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class User {
+  final int id;
+  final String name;
+  final String email;
+  final String avatarUrl;
+
+  const User({
+    this.id,
+    @required this.name,
+    @required this.email,
+    @required this.avatarUrl,
+  });
+}
